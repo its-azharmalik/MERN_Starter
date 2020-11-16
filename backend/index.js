5c420eb0e94523177365709579a022071f485e9d
Azhar Malik <malik_258@Azhars-MacBook-Pro.local>
2020-11-16 14:57:03 +0530

    commiting all changes including the node modules and stuff

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from cors;
 
+const express = require('express')
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded ({ limit: "30mb", extended: true}))
+app.use(cors())
+
+//Start here by connecting cloud atlas mongo db
