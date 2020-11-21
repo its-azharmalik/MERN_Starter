b9bf19dc920f99611f303ded093c7b3d77bfd09d
Azhar Malik <malik_258@Azhars-MacBook-Pro.local>
2020-11-21 15:24:14 +0530

    Finalized and Updated was having some error in Synax in the Index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
-import cors from cors;
+import cors from 'cors';
 
-const express = require('express')
+const app = express();
+
+console.log('Server is Running at Port 3000')
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded ({ limit: "30mb", extended: true}))
