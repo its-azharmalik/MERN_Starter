abba2a3d5a517d7cd8eeaf2ca74d4cea84454174
Azhar Mahmood <i19cy023@chem.svnit.ac.in>
2021-10-16 12:56:52 +0530

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,17 +7,7 @@ const HomeScreen = () => {
     return (
     <>
         <div className='home-screen'>
-            <div className='recent-post-heading'>
-                <h2>Recent Posts</h2>
-            </div>
-            <div className='post-button'>
-               <Link to='/postmemory'>
-                    <button>Post Button</button>
-               </Link> 
-            </div>
-            <div className='user-name'>
-                <p>signed in as <strong>@malik_258</strong></p>
-            </div>
+            <h1> This is Home Screen </h1>
         </div>
 
         
