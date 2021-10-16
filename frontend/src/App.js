be7390305ccf63485b90a8f2af2a920ad0f7d16f
Azhar Mahmood <i19cy023@chem.svnit.ac.in>
2021-10-16 12:54:40 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from './Components/Navbar Components/Navbar'
 import HomeScreen from './screens/HomeScreen'
 import './App.css'
-import Posts from './Components/Posts Components/Posts'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 
